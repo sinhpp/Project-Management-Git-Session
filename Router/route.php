fff529aa5f2fe7c8b26fe927c4f3321ca05fe5a9
mesa nat <mesa.nath@student.passerellesnumeriques.org>
2025-03-03 08:44:21 +0700

    feat(added user)

--- Router/route.php
+++ Router/route.php
@@ -4,10 +4,12 @@ require_once "Controllers/BaseController.php";
 require_once "Database/Database.php";
 require_once "Controllers/WelcomeController.php";
 require_once "Controllers/TableController.php";
+require_once "Controllers/ManageUserController.php";
 
 
 $route = new Router();
 $route->get("/", [WelcomeController::class, 'welcome']);
 $route->get("/table-list", [TableController::class, 'showTable']);
+$route->get("/user", [ManageUserController::class, 'show']);
 
 $route->route();
