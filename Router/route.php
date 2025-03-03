af486ca995539fb0010ebc80c3a26e22fca57a87
yon ratana <yonratana71@gmail.com>
2025-03-03 09:20:35 +0700

    feat(add user): added ManageUser

--- Router/route.php
+++ Router/route.php
@@ -5,10 +5,12 @@ require_once "Database/Database.php";
 require_once "Controllers/WelcomeController.php";
 require_once "Controllers/TableController.php";
 require_once "Controllers/StoryController.php";
+require_once "Controllers/ManageUserController.php";
 
 $route = new Router();
 $route->get("/", [WelcomeController::class, 'welcome']);
 $route->get("/table-list", [TableController::class, 'showTable']);
+$route->get("/user", [ManageUserController::class, 'show']);
 
 $route->get("/story", [StoryController::class, 'story']);
 
